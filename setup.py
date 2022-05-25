55bfe3a9f6f58c16e7f285f5ad68284307041fa4
florpi <florpisoteadadejardin@gmail.com>
2022-05-25 10:37:59 +0100

    added requirements

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
 from distutils.core import setup
+from os.path import abspath, dirname, join
+
+this_dir = abspath(dirname(__file__))
+with open(join(this_dir, "requirements.txt")) as f:
+    requirements = f.read().split("\n")
 
 setup(
     name='JaxHankel',
@@ -9,18 +14,7 @@ setup(
     packages=['jax_fht'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
-
-    install_requires=[
-                    'numpy',
-                    'jupyter',
-                    'h5py',
-                    'pandas',
-                    'scipy',
-                    'seaborn',
-                    'matplotlib',
-                    'halotools',
-                    ],
-
+    install_requires=requirements,
     zip_safe=False
 )
 
