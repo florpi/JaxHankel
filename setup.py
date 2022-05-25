b57fe4527480ee3188cff82864fdaae4eb721fef
florpi <florpisoteadadejardin@gmail.com>
2022-05-25 11:58:32 +0200

    new version

--- setup.py
+++ setup.py
@@ -9,11 +9,12 @@ setup(
     name='JaxHankel',
     author="Carolina Cuesta-Lazaro",
     author_email="carolina.cuesta-lazaro@durham.ac.uk",
-    version='0.1dev',
+    version='0.1',
     url='https://github.com/florpi/JaxHankel',
     packages=['jax_fht'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     install_requires=requirements,
     zip_safe=False
 )
